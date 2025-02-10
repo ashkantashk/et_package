7bc0471e7ad3d92db82fd0bcd748024df603ee70
Ashkan Tashk <53473481+ashkantashk@users.noreply.github.com>
2025-02-10 22:33:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     description='A package for ET_Func with gaze data processing',
     author='ashta',
-    packages=['et_package'],,
+    packages=['et_package'],
     install_requires=[
         'pandas',
         'numpy==1.26.4',
